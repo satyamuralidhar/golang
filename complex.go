2c5d5e9ba3e2155432275c04a2097008541a6a6a
satyamuralidhar <immurali5@gmail.com>
2019-12-24 05:15:06 -0800

    complex

---
+++ complex.go
@@ -0,0 +1,21 @@
+//imaginary 
+
+
+package main
+
+import "fmt"
+
+func main() {
+
+         k1 := complex(5,7)
+         k2 := 7+10i
+
+        k  := k1+ k2
+        
+ 	fmt.Println("addition of complex numbers: " , k)
+
+        fmt.Printf("type of k1 is %T \n" , k1)
+ 
+	fmt.Printf("type of k2 is %Ti \n" , k2)
+
+}
