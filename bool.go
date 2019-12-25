cf14eef27d677fb5b36fa10e58f8e07061d62941
satyamuralidhar <immurali5@gmail.com>
2019-12-24 21:28:12 -0800

    boolean

---
+++ bool.go
@@ -0,0 +1,16 @@
+package main 
+
+import "fmt"
+
+func main() {
+
+	n := 1==1
+	
+	m := 1==5
+
+	fmt.Printf("%v ,type of n is %T \n" , n , n)
+
+	fmt.Printf("%v ,type of m is %T \n" , m , m)
+
+
+}
---
+++ float.go
@@ -0,0 +1,16 @@
+
+package main
+
+import "fmt"
+
+func main() { 
+
+	i := 10
+        
+	j := 10.9
+
+	sum := i+int(j)
+
+	fmt.Println(sum)
+
+}
