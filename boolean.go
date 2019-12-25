50ef586edafb8e7cf2d7a834c04f85dcbcdd9dbf
satyamuralidhar <immurali5@gmail.com>
2019-12-24 21:40:36 -0800

    boolean

--- boolean.go
+++ boolean.go
@@ -4,13 +4,15 @@ import "fmt"
 
 func main() {
 
-	n := 1==1
+	n := 0
 	
-	m := 1==5
-
+	m:=1  
+	
 	fmt.Printf("%v ,type of n is %T \n" , n , n)
 
 	fmt.Printf("%v ,type of m is %T \n" , m , m)
 
-
+	fmt.Println("n==m",n==m)
+	
+	fmt.Println("n!=m",n!=m)
 }
