3e6f1ed7937fb17b4d98b8baef53f21c34ea9e91
satyamuralidhar <immurali5@gmail.com>
2019-12-27 12:01:42 +0530

    sample array

---
+++ array/sample.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a [5]int
+	a[0] = 10
+	a[1] = 20
+	a[2] = 30
+	a[3] = 40
+	a[4] = 50
+	fmt.Println(a)
+}
---
+++ array/shorthand.go
@@ -0,0 +1,6 @@
+package main
+import "fmt"
+func main() {
+	a :=[5] int {10 , 20 , 30 , 40 , 50}
+	fmt.Println(a)
+}
