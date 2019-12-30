4e2a796a6ccb7e6ab7484fdbf69468329be9ffdc
satyamuralidhar <immurali5@gmail.com>
2019-12-30 15:06:54 +0530

    struct

---
+++ functions/struct.go
@@ -0,0 +1,11 @@
+package main
+import "fmt"
+type Student struct {
+	name string
+	rollno int
+	percentage float64
+}
+func main() {
+	var satya = Student{"satya muralidhar",458504800,72.6}
+	fmt.Println(satya)
+}
