6c2854484b90aeee98afdc81ef83fc269d032f8c
satyamuralidhar <immurali5@gmail.com>
2019-12-30 12:17:56 +0530

    string

---
+++ array/string.go
@@ -0,0 +1,14 @@
+package main
+import ("fmt"
+)
+func countries() []string {
+	countries := []string{"india","china","america","canada","italy"}
+	countryslice := countries[:len(countries)-2]
+	countrycopy :=make([]string,len(countryslice))
+	copy(countrycopy , countryslice)
+	return countrycopy
+}
+func main() {
+countryneed := countries()
+fmt.Println(countryneed)
+}
