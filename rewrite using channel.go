a64ef98aead452fb84e6fdb3320e0d4cd7042cfb
satyamuralidhar <immurali5@gmail.com>
2020-01-03 16:20:07 +0530

    rewrite

---
+++ rewrite using channel.go
@@ -0,0 +1,30 @@
+package main 
+import ("fmt"
+		"time"
+)
+func sample() {
+	fmt.Println("hi")
+}
+func main() {
+	go sample()
+	time.Sleep(2 * time.Second)
+	fmt.Println("exit")
+
+}
+
+//rewrite above program using channnel
+
+package main
+import "fmt"
+func sample(done chan bool) {
+	fmt.Println("hi")
+	done <- true
+
+}
+func main() {
+	done := make(chan bool)
+	go sample(done)
+	<- done
+	fmt.PrintlnI(exit)
+
+}
